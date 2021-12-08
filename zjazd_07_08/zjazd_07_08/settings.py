f52163337d12e96c86aad8ad769a0569ca2b3c62
1piotrnowicki1 <piotr.nowick92@gmail.com>
2021-12-08 20:29:48 +0100

    setting

--- zjazd_07_08/zjazd_07_08/settings.py
+++ zjazd_07_08/zjazd_07_08/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from zjazd_07_08.settings_local import *
+except ImportError:
+    pass
+
+
---
+++ zjazd_07_08/zjazd_07_08/settings_local.py
@@ -0,0 +1,12 @@
+DEBAG = True
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'HW7/8',
+       'USER': 'postgres',
+       'PASSWORD': 'password',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   },
+}
